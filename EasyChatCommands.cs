3f1eab007bd32af60d61672299ac91f06ddcf393
Jake Rich <weirdo44@hotmail.com>
2017-05-26 15:37:53 -0400

    A few more plugins on oxide

---
+++ EasyChatCommands.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oxide.Plugins
+{
+    [Info("Easy Chat Commands", "Jake Rich", "1.0.0")]
+    [Description("Easy add custom info commands")]
+
+    class EasyChatCommands : RustPlugin
+    {
+        public Dictionary<string, string> chatCommands { get; set; }
+        private const string configFileName = "EasyChatCommands";
+
+        protected override void LoadDefaultConfig()
+        {
+            PrintWarning("Creating a new configuration file");
+            Config.Clear();
+            chatCommands = new Dictionary<string, string>();
+            chatCommands.Add("examplemultilinecommand", "Use \n to create a newline. This is line1\nThis is line2.");
+            chatCommands.Add("exampleinfocommand", "Example Reply, You need a comma here -->");
+            chatCommands.Add("exampleinfocommand2", "No comma on the last item -->");
+            foreach(KeyValuePair<string,string> keyValue in chatCommands)
+            {
+                Config[keyValue.Key] = keyValue.Value;
+            }
+            Config.Save();
+        }
+
+        void Init()
+        {
+            try
+            {
+                chatCommands = Config.ReadObject<Dictionary<string, string>>();
+            }
+            catch (Exception ex)
+            {
+                Puts($"ERROR: Config file is formatted incorrectly! EasyChatCommands are NOT LOADED!!!");
+                return;
+            }
+            foreach(string command in chatCommands.Keys)
+            {
+                cmd.AddChatCommand(command, this, "OnChatInfoCommand");
+                Puts("Added a command");
+            }
+            Puts("Loaded Chat Commands successfully.");
+        }
+
+        void OnChatInfoCommand(BasePlayer player, string command)
+        {
+            string reply;
+            if (chatCommands.TryGetValue(command, out reply))
+            {
+                PrintToChat(player, reply);
+            }
+        }
+
+    }
+}
---
+++ QueueHolder.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("QueueHolder", "Jake_Rich", 1.0)]
+    [Description("Saves your position in queue if you disconnect")]
+
+    public class QueueHolder : RustPlugin
+    {
+        public static QueueHolder _plugin { get; set; }
+
+        public Dictionary<ulong, QueueData> queueData { get; set; } = new Dictionary<ulong, QueueData>();
+
+        public const float timeToHoldSpot = 5; //Minutes
+
+        public class QueueData
+        {
+            public ulong userID { get; set; }
+            public DateTime disconnectTime { get; set; } = new DateTime();
+
+            public QueueData()
+            {
+
+            }
+
+            public QueueData(ulong id)
+            {
+                userID = id;
+            }
+        }
+
+        void Loaded()
+        {
+            _plugin = this;
+        }
+
+        void OnPlayerDisconnected(BasePlayer player, string reason)
+        {
+            var data = GetQueueData(player);
+            data.disconnectTime = DateTime.Now;
+        }
+
+        public QueueData GetQueueData(BasePlayer player)
+        {
+            return GetQueueData(player.userID);
+        }
+
+        public QueueData GetQueueData(ulong player)
+        {
+            QueueData data;
+            if (!queueData.TryGetValue(player, out data))
+            {
+                data = new QueueData(player);
+                queueData[player] = data;
+            }
+            return data;
+        }
+
+        object CanBypassQueue(Network.Connection connection)
+        {
+            if (connection.authLevel > 0)
+            {
+                return true;
+            }
+            var data = GetQueueData(connection.userid);
+            if (DateTime.Now.Subtract(data.disconnectTime).TotalSeconds < timeToHoldSpot * 60f)
+            {
+                return true;
+            }
+            return null;
+        }
+    }
+}
+
+
